8ee013426c5044373a1fb4fa0c7d7dcf4f645d47
haydenpham <hayden.pham01@gmail.com>
2024-07-01 21:37:17 -0300

    Added basic functionalities
    Added core HTML, CSS, and TS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,55 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'LeetCodeSolutionFileNameGenerator';
+  difficulty: string = 'E';
+  question: string = '';
+  selectedLanguage: string | null = null;
+  fileName: string | null = null;
+
+  languages = [
+    { name: 'C#', code: 'C#' },
+    { name: 'Java', code: 'J' },
+    { name: 'Python', code: 'Py' },
+    { name: 'SQL', code: 'SQL' },
+    { name: 'Bash Script', code: 'B' }
+  ];
+
+  generateFileName() {
+    if (!this.selectedLanguage) {
+      alert('Please select a programming language.');
+      return;
+    }
+
+    if (!this.isValidLeetCodeQuestionFormat(this.question)) {
+      alert('Please enter a valid LeetCode question format.');
+      this.clear();
+      return;
+    }
+
+    const formattedQuestion = this.formatQuestion(this.question);
+    this.fileName = `[${this.difficulty}][${this.selectedLanguage}].${formattedQuestion}`;
+  }
+
+  private formatQuestion(question: string): string {
+    const [number, ...titleParts] = question.split('. ');
+    const formattedTitle = titleParts.join(' ').replace(/\s+/g, ' ')
+      .split(' ')
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+      .join('');
+    return `${number}.${formattedTitle}`;
+  }
+
+  isValidLeetCodeQuestionFormat(input: string): boolean {
+    const pattern = /^\d+\.\s[A-Za-z]+(\s[A-Za-z]+)*$/;
+    return pattern.test(input);
+  }
+
+
+  // TODO: Add clear button to reset form
+  clear() {
+    this.difficulty = 'E';
+    this.question = '';
+    this.selectedLanguage = null;
+    this.fileName = null;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -11,11 +19,21 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [
     provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  
+}
