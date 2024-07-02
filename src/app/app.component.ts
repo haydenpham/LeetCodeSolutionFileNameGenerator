20c2be5cf1092347cf99a69a150084abde6d4384
haydenpham <hayden.pham01@gmail.com>
2024-07-02 19:01:21 -0300

    Add CSS, Finalize the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   difficulty: string = 'E';
   question: string = '';
-  selectedLanguage: string | null = null;
   fileName: string | null = null;
 
   languages = [
-    { name: 'C#', code: 'C#' },
-    { name: 'Java', code: 'J' },
-    { name: 'Python', code: 'Py' },
-    { name: 'SQL', code: 'SQL' },
-    { name: 'Bash Script', code: 'B' }
+    { name: 'Java', value: 'J', logo: 'https://upload.wikimedia.org/wikipedia/en/3/30/Java_programming_language_logo.svg' },
+    { name: 'C#', value: 'C#', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Logo_C_sharp.svg/1820px-Logo_C_sharp.svg.png' },
+    { name: 'Python', value: 'Py', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png' },
+    { name: 'SQL', value: 'SQL', logo: 'https://upload.wikimedia.org/wikipedia/commons/8/87/Sql_data_base_with_logo.png' },
+    { name: 'Bash Script', value: 'B', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Bash_Logo_Colored.svg/2048px-Bash_Logo_Colored.svg.png' },
   ];
+
+  selectedLanguage: string | null = this.languages[0].value;
 
   generateFileName() {
     if (!this.selectedLanguage) {
